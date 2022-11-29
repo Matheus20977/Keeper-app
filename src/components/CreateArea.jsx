48c56f3842c892f4ee13031b90f86d06ea1cf8e7
Matheus20977 <matheus20977@gmail.com>
2022-11-29 11:35:38 -0300

    more code cleaning and add some styling with material ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+  const [expanded, setExpanded] = useState(false);
+
   const [noteText, setNoteText] = useState({
       title: "",
       content: ""
@@ -17,19 +22,41 @@ function CreateArea(props) {
     });
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input onChange={handleChange} name="title" placeholder="Title" value={noteText.title} />
-        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={noteText.content} />
-        <button onClick={(event) => {
-          props.onAdd(noteText);
-          setNoteText({   
-            title: "",
-            content: ""
-          });
-          event.preventDefault();
-        }}>Add</button>
+      <form className="create-note">
+        {expanded && <input 
+          onChange={handleChange} 
+          name="title" 
+          placeholder="Title" 
+          value={noteText.title} 
+        />}
+        <textarea 
+          onChange={handleChange} 
+          onClick={expand}
+          name="content" 
+          placeholder="Take a note..." 
+          rows={expanded ? "3" : "1"}
+          value={noteText.content} 
+        />
+        <Zoom in={expanded}>
+          <Fab 
+            onClick={(event) => {
+              props.onAdd(noteText);
+              setNoteText({   
+                title: "",
+                content: ""
+              });
+              event.preventDefault();
+            }}
+          >
+            <NoteAddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
